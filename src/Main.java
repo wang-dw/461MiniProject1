1242a7a55c5fae3d321bb4289676939292ffdef3
wang-dw <wangdw@miamioh.edu>
2018-04-11 15:57:15 -0400

    Changes

--- src/Main.java
+++ src/Main.java
@@ -9,14 +9,14 @@ import java.util.*;
 
 public class Main {
 
-	static int packets = 100000;
+	static int packets = 1000000;
 	static double lambda = 8;
 	static int mu1 = 5;
 	static int mu2 = 5;
-	static double phi = .5;
-	final static int MAXBUFFER = 6;
+	static double phi = .4;
+	final static int MAXBUFFER = 5;
 
-	public static int getPoissonRandom(double mean) {
+	public static double getPoissonRandom(double mean) {
 		Random r = new Random();
 		double L = Math.exp(-mean);
 		int k = 0;
@@ -42,12 +42,21 @@ public class Main {
 
 	public static double serviceTime(double mean) {
 		Random rand = new Random();
-		return Math.log(1 - rand.nextDouble()) / (-(1 / mean));
+		return Math.log(1 - rand.nextDouble()) / (-(mean));
 	}
+
+	/*
+	 * public static double removeQueue(PriorityQueue<Double> queue, double
+	 * timePassed, double arrival) { while (queue.size() > 0 && timePassed <=
+	 * arrival) { timePassed += queue.peek(); queue.poll(); } if (timePassed >
+	 * arrival) { queue.add(timePassed - arrival); return (timePassed - arrival); }
+	 * else { return 0.0; } }
+	 */
 
 	public static void main(String[] args) {
 		PriorityQueue<Double> queue1 = new PriorityQueue<Double>();
 		PriorityQueue<Double> queue2 = new PriorityQueue<Double>();
+
 		double blocked1 = 0;
 		double blocked2 = 0;
 		double total1 = 0;
@@ -55,50 +64,47 @@ public class Main {
 		double delay1 = 0;
 		double delay2 = 0;
 
-		double timePassed1 = 0;
-		double timePassed2 = 0;
-
 		for (int i = 0; i < packets; ++i) { // go through each packet
-			double arrival = getPoissonRandom(1 / lambda); // arrival time of packet
+			double arrival = 1 / getPoissonRandom(lambda); // arrival time of packet
+			double timePassed1 = 0;
+			double timePassed2 = 0;
 
-			// calculate when to remove from queue, if enough time pass, remove from queue
-			if (queue1.size() > 0) {
-				if (timePassed1 > queue1.peek()) {
-					timePassed1 -= queue1.poll();
-					delay1 += timePassed1;
-				}
-				timePassed1 += arrival;
-			} else {
-				timePassed1 = 0;
-			}
-
-			// calculate when to remove from queue, if enough time pass, remove from queue
-			if (queue2.size() > 0) {
-				if (timePassed2 > queue2.peek()) {
-					timePassed2 -= queue2.poll();
-					delay2 += timePassed2;
-				}
-				timePassed2 += arrival;
-			} else {
-				timePassed2 = 0;
-			}
-
-			if (i > 10000) {
+			if (i > 100000) {
 				// packet arrive, add it to a queue
 				if (chooseQueue()) {
 					total1++;
 					if (queue1.size() <= MAXBUFFER) {
-						queue1.add(serviceTime(1 / mu1));
+						queue1.add(serviceTime(mu1));
 					} else {
 						blocked1++;
 					}
 				} else {
 					total2++;
 					if (queue2.size() <= MAXBUFFER) {
-						queue2.add(serviceTime(1 / mu2));
+						queue2.add(serviceTime(mu2));
 					} else {
 						blocked2++;
 					}
+				}
+			}
+
+			// calculate when to remove from queue, if enough time pass, remove from queue
+			while (queue1.size() > 0 && timePassed1 < arrival) {
+				timePassed1 += queue1.peek();
+				delay1 += queue1.peek();
+				queue1.poll();
+				if (timePassed1 > arrival) {
+					queue1.add(timePassed1 - arrival);
+				}
+			}
+
+			// calculate when to remove from queue, if enough time pass, remove from queue
+			while (queue2.size() > 0 && timePassed2 < arrival) {
+				timePassed2 += queue2.peek();
+				delay2 += queue2.peek();
+				queue2.poll();
+				if (timePassed2 > arrival) {
+					queue2.add(timePassed2 - arrival);
 				}
 			}
 
@@ -127,8 +133,10 @@ public class Main {
 		System.out.println("Throughput 2: " + throughputQ2);
 		System.out.println("Throughput System: " + throughputAll);
 		System.out.println("Average Packets 1: " + averagePacketsQ1);
-		System.out.println("Averqage Packets 2: " + averagePacketsQ2);
-		System.out.println("Averqage Packets System: " + averagePacketsAll);
+		System.out.println("Average Packets 2: " + averagePacketsQ2);
+		System.out.println("Average Packets System: " + averagePacketsAll);
+		System.out.println(1/getPoissonRandom(lambda));
+		System.out.println(serviceTime(mu1));
 	}
 
 }
